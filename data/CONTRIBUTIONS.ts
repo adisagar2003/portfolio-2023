2bcb16fe432df65a6d145efee3f0b76afab7fc82
adisagar2003 <adityasagarpython@gmail.com>
2023-05-26 10:09:00 -0230

    Deploying

---
+++ data/CONTRIBUTIONS.ts
@@ -0,0 +1,21 @@
+import { projectCardProps } from "../components/contribution";
+
+export const CONTRIBUTIONS:Array<projectCardProps> = [
+    {
+        projectName:'Mastodon',
+        projectDescription:'Reviewed 1 pull request in 1 repository',
+        gitHubLink:'https://mastodon.social/explore',
+        liveLink:'https://mastodon.social/explore'
+
+    },{
+        projectName:'e-LearningDAO',
+        projectDescription:'Cleaning up UI, setting up new UI for wallet section',
+        liveLink:'https://app.pocre.net',
+        gitHubLink:'https://github.com/eLearningDAO/POCRE'
+    },{
+        projectName:'MUN Computer Science Society',
+        projectDescription:'Adding Teaching Excellence Award section',
+        liveLink:'https://muncompsci.ca/',
+        gitHubLink:'https://github.com/MUNComputerScienceSociety/MUNCS-Website'
+    }
+]
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { AiFillLinkedin, AiOutlineLinkedin } from 'react-icons/ai'
 
 const config: DocsThemeConfig = {
   logo: <span>Aditya</span>,
@@ -7,12 +8,34 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/adisagar2003',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.linkedin.com/in/aditya-sagar-42948721a/',
+    icon: (
+     <AiOutlineLinkedin style={{width:30, height:30}} />
+    )
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Made by Aditya',
   },
+  banner: {
+    key: '2.0-release',
+    text: (
+      <a href="https://github.com/adisagar2003/PERN-Blog" target="_blank">
+      <u> 🎉 Please give a star if you like this portfolio on GitHub</u> 
+      </a>
+    )
+  },
+  editLink:{
+    component:null,
+    text: null
+  },
+  feedback:{
+    content:null,
+    labels:null
+  },
+  search:{
+    placeholder:'search content..'
+  }
 }
 
 export default config
