2ed93ba2015218553a87402ce752da0845ad9b40
adisagar2003 <adityasagarpython@gmail.com>
2023-05-26 09:10:13 -0230

    feat: :fire: Removed External
    Links

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: '',
   footer: {
     text: 'Made by Aditya',
   },
